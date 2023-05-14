1adb981b8c2b2434975ebf8bd7a0482250b216a0
Somesh Kumar <kumarsomesh.unos@gmail.com>
2023-05-15 00:34:21 +0530

    basic setup complete

--- frontend-part/src/App.js
+++ frontend-part/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './components/NavigationBar'
+import About from './pages/About'
+import Home from './pages/Home'
+import Sponsors from './pages/Sponsors'
+import Teams from './pages/Teams'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route exact path='/about' element={<About />}></Route>
+        <Route exact path='/' element={<Home />}></Route>
+        <Route exact path='/sponsors' element={<Sponsors />}></Route>
+        <Route exact path='/teams' element={<Teams />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
