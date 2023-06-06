7f02bb350bd3b7d1c07854ba74a55c572ae2f628
Shubham <shubham.219301072@muj.manipal.edu>
2023-06-06 14:37:49 +0530

    #b3 bug fixes

--- backend-part/routes/swj2023.py
+++ backend-part/routes/swj2023.py
@@ -5,31 +5,46 @@ from datetime import datetime
 import json
 import uuid
 import pandas as pd
-from models.swj2023 import database
-from mailer.swj2023 import mailer
+from models import database
+from mailers import mailer
+
+# Create a swj2023 route
+swj2023 = Blueprint('swj2023', __name__)
+
+"""
+LIST OF ROUTES:
+-> /
+-> /admin
+-> /read
+-> /download
+-> /create
+-> /event/<uuid>
+-> /scan
+-> /create-client-qr
+-> /file/QR/<qr_file_name>
+"""
 
 
-# Create a blueprint for /swj2023 route
-swj2023_routes = Blueprint('swj2023', __name__)
-
-
-@swj2023_routes.route('/')
+# swj2023 index route
+@swj2023.route('/')
 def index():
     return {
-        "message": "Welcome to SWJ 2023!",
+        "message": "Welcome to Welcome to Startup Weekend Jaipur 2023!",
         "fulfilled": True
     }
 
 
-@swj2023_routes.route('/admin')
+# route to render the admin page
+@swj2023.route('/admin')
 def admin():
     return render_template("adminPanel.html")
 
 
-@swj2023_routes.route('/read', methods=['GET'])
+# route to fetch records from the database
+@swj2023.route('/read', methods=['GET'])
 def read():
     password = request.args.get('password')
-    data = database.getRecords(password, access="ADMIN")
+    data = database.getRecords(password, access="READ")
     if data:
         return {
             "message": "Read Request Successful!",
@@ -40,13 +55,15 @@ def read():
         abort(401, 'Error: Unauthorized, incorrect password!')
 
 
-@swj2023_routes.route('/download', methods=['GET'])
+# route to download records in form of Excel
+@swj2023.route('/download', methods=['GET'])
 def download():
     password = request.args.get('password')
     data = database.getRecords(password, access="DOWNLOAD")
     if data:
-        df = pd.DataFrame(data, columns=['Uuid', 'Name', 'Phone', 'Email', 'Workplace', 'City', 'Age', 'Gender', 'Transaction ID', 'Time'])
-        df = df.drop('Time', axis=1)
+        df = pd.DataFrame(data, columns=['Uuid', 'Name', 'Phone', 'Email', 'Workplace', 'City', 'Age', 'Gender',
+                                         'Transaction ID', 'Mail Status', 'Time'])
+        df = df.drop(['Time', 'Mail Status'], axis=1)
         current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         excel_file = f"SWJ_{current_datetime}.xlsx"
         df.to_excel(excel_file, index=False)
@@ -57,24 +74,26 @@ def download():
         abort(401, 'Error: Unauthorized, incorrect password!')
 
 
-@swj2023_routes.route('/create', methods=['POST'])
+# route to create a participant entry into the database
+@swj2023.route('/create', methods=['POST'])
 async def create():
     data = request.get_data().decode('utf-8')
-    p_uuid = str(uuid.uuid4())
-    p_name = json.loads(data)['name']
-    p_mob_num = json.loads(data)['mob_num']
-    p_email = json.loads(data)['email']
-    p_workplace = json.loads(data)['workplace']
-    p_city = json.loads(data)['city']
-    p_age = json.loads(data)['age']
-    p_gender = json.loads(data)['gender']
-    p_transaction_id = json.loads(data)['transaction_id']
+    uuid_ = str(uuid.uuid4())
+    name = json.loads(data)['name']
+    phone = json.loads(data)['phone']
+    email = json.loads(data)['email']
+    work_place = json.loads(data)['workPlace']
+    city = json.loads(data)['city']
+    age = json.loads(data)['age']
+    gender = json.loads(data)['gender']
+    transaction_id = json.loads(data)['transactionId']
 
     # check if all the data parameters received or not
-    if not (p_name and p_mob_num and
-            p_email and p_workplace and
-            p_city and p_age and
-            p_gender and p_transaction_id
+    if not (
+        name and phone and
+        email and work_place and
+        city and age and
+        gender and transaction_id
     ):
         # abort if some parameters are missing
         abort(400, 'Error: Missing required parameters!')
@@ -82,29 +101,30 @@ async def create():
 
     # check if email syntax is correct or not
     email_pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
-    if not match(email_pattern, p_email):
+    if not match(email_pattern, email):
         abort(422, 'Error: Email validation failed!')
         return
 
     # check if email, phone and transaction ID is unique or not
-    if (database.isEmailUnique(p_email) or
-        database.isMobileNumberUnique(p_mob_num) or
-        database.isTransactionIdUnique(p_transaction_id)
+    if (
+        database.isEmailUnique(email) or
+        database.isMobileNumberUnique(phone) or
+        database.isTransactionIdUnique(transaction_id)
     ):
         abort(409, 'Error: Conflict, ensure that email, mobile, and transaction ID are unique!')
         return
 
     # if everything goes fine then insert the record into the database
     record = {
-        "p_uuid": p_uuid,
-        "p_name": p_name,
-        "p_mob_num": p_mob_num,
-        "p_email": p_email,
-        "p_workplace": p_workplace,
-        "p_city": p_city,
-        "p_age": p_age,
-        "p_gender": p_gender,
-        "p_transaction_id": p_transaction_id
+        "uuid": uuid_,
+        "name": name,
+        "phone": phone,
+        "email": email,
+        "workPlace": work_place,
+        "city": city,
+        "age": age,
+        "gender": gender,
+        "transactionId": transaction_id
     }
     try:
         database.createRecord(record)
@@ -112,35 +132,37 @@ async def create():
         # send the mail to the participant
         if mailer.sendMail(record):
             # update to the database that mail has been sent
-            database.setMailSent(p_uuid, status=True)
+            database.setMailSentToTrue(uuid_)
 
         return {
             "message": "Data Successfully Inserted!",
             "fulfilled": True,
-            "uuid": p_uuid
+            "uuid": uuid_
         }
     except Exception as e:
         abort(500, 'Error: Database error!', e)
 
 
-@swj2023_routes.route('/event/<uuid_>', methods=['GET'])
+# route to get the participant detail using their uuid
+@swj2023.route('/event/<uuid_>', methods=['GET'])
 def event(uuid_):
     # return render_template('clientQR.html', uuid_=uuid_)
     data = database.getParticipantInfo(uuid_)
     if data:
         return {
             "message": "Data retrieved successfully!",
-            "p_uuid": data[0],
-            "p_name": data[1],
-            "p_mob_num": data[2],
-            "p_email": data[3],
+            "uuid": data[0],
+            "name": data[1],
+            "phone": data[2],
+            "email": data[3],
             "fulfilled": True
         }
     else:
         abort(404, 'Error: Data not found!')
 
 
-@swj2023_routes.route('/scan')
+# route to render the scanner page
+@swj2023.route('/scan')
 def scan():
     password = request.args.get('password')
     if database.isPasswordCorrect(password, access="SCAN"):
@@ -149,15 +171,17 @@ def scan():
         abort(401, 'Error: Unauthorized, incorrect password!')
 
 
-@swj2023_routes.route('/create-client-qr')
+# route to render the client-qr page this page will create QR codes for clients
+@swj2023.route('/create-client-qr')
 def createClientQR():
     uuid_ = request.args.get('uuid')
     return render_template("clientQR.html", uuid_=uuid_)
 
 
-@swj2023_routes.route('/file/QR/<qr_file_name>')
+# route to get the png file or qr code using participant's uuid
+@swj2023.route('/file/QR/<qr_file_name>')
 def returnQRFile(qr_file_name):
-    file_path = f"./data/swj2023/QRs/{qr_file_name}"
+    file_path = f"./data/QRs/{qr_file_name}"
     if path.exists(file_path):
         return send_file(file_path, mimetype='image/png')
     else:
