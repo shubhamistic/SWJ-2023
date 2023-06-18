7901af9c0cbfddae2a06618960f5a140c6558f57
Samarth Bagga <samarthbagga@gmail.com>
2023-06-18 21:42:03 +0530

    sponsors page

--- frontend-part/src/data/sponsors.js
+++ frontend-part/src/data/sponsors.js
@@ -1,54 +1,44 @@
 let sponsorsData = [
     {
-        image: 'https://picsum.photos/200/300',
-        desc: 'title',
-        link: 'https://www.fernhotels.com/',
-        name: "Title here"
+        name: "Google",
+        link: "https://www.google.com/",
+        image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg"
     },
-
-    {
-        image: 'https://picsum.photos/200/300',
-        desc: 'title',
-        link: 'https://www.dell.com/en-in',
-        name: "Title here"
-    },
-
-    {
-        image: 'https://picsum.photos/200/300',
-        desc: 'title',
-        link: 'dulux.in',
-        name: "Title here"
-    },
-    {
-        image: 'https://picsum.photos/200/300',
-        desc: 'title',
-        link: 'https://www.elitetechnogroups.com/',
-        name: "Title here"
-    },
-    {
-        image: 'https://picsum.photos/200/300',
-        desc: 'title',
-        link: 'https://www.heromotocorp.com/en-in/homepage.html',
-        name: "Title here"
-    },
-    {
-        image: 'https://picsum.photos/200/300',
-        desc: 'title',
-        link: 'https://www.dell.com/en-in',
-        name: "Title here"
-    },
-    {
-        image: 'https://picsum.photos/200/300',
-        desc: 'title',
-        link: 'https://www.dell.com/en-in',
-        name: "Title here"
-    },
-    {
-        image: 'https://picsum.photos/200/300',
-        desc: 'title',
-        link: 'https://www.dell.com/en-in',
-        name: "Title here"
-    },
+      {
+        name: "Amazon",
+        link: "https://www.amazon.com/",
+        image: "https://logowik.com/content/uploads/images/amazon6707.jpg"
+      },
+      {
+        name: "Microsoft",
+        link: "https://www.microsoft.com/",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/2048px-Microsoft_logo.svg.png"
+      },
+      {
+        name: "Apple",
+        link: "https://www.apple.com/",
+        image: "https://1000logos.net/wp-content/uploads/2016/10/Apple-Logo.png"
+      },
+      {
+        name: "Facebook",
+        link: "https://www.facebook.com/",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png"
+      },
+      {
+        name: "Tesla",
+        link: "https://www.tesla.com/",
+        image: "https://a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2020/06/1200/675/TESLA-LOGO.jpg?ve=1&tl=1"
+      },
+      {
+        name: "Berkshire Hathaway",
+        link: "https://www.berkshirehathaway.com/",
+        image: "https://www.freepnglogos.com/uploads/berkshire-hathaway-logo-png/berkshire-hathaway-logo-home-berkshire-hathaway-homeservices-new-mexico-33.png"
+      },
+      {
+        name: "JPMorgan Chase",
+        link: "https://www.jpmorganchase.com/",
+        image: "https://media2.vault.com/14343503/210909_jp-morgan_logo.jpg"
+      }
 
 ]
 
--- frontend-part/src/pages/Home.jsx
+++ frontend-part/src/pages/Home.jsx
@@ -6,22 +6,22 @@ const Home = () => {
   return (
 <section className='hero'>
 <div className='heroContainer'>
-            <ParticlesBg/>
-            <div className='heroImage'>
-
-            </div>
-            <div className='heroTitle'>
-              Startup Weekend Jaipur
-            </div>
+        <div className='swjTitle'>
+        A three-day collaborative and creative workshop
         </div>
+        </div>
+        <div className='homePageStuff'>
+          <div className='homePageInnerStuff'>
       <div className='first'>
         <div className='img1'>
 
         </div>
         <div className='text1'>
           <div className='heroBox'>
-         <div className='heroBoxTitle'><h1>Lorem Ipsum Dolor</h1></div>
-         <div><p>Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum DolorLorem Ipsum DolorLorem Ipsum Dolor Lorem Ipsum Dolor</p></div>
+         <div className='heroBoxTitle'><h1>Day 1</h1></div>
+         <div><p>
+         Began with a motivational speech from a professional in the field, laying the groundwork for the upcoming entrepreneurial journey. The event "Break the Ice" promoted a lively environment for participants to network, form teams, and make contacts with others
+          </p></div>
         </div>
 
         </div>
@@ -29,8 +29,8 @@ const Home = () => {
       <div className='three'>
         <div className='text2'>
         <div className='heroBox'>
-         <div className='heroBoxTitle'><h1>Lorem Ipsum Dolor</h1></div>
-         <div><p>Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum DolorLorem Ipsum DolorLorem Ipsum Dolor Lorem Ipsum Dolor</p></div>
+         <div className='heroBoxTitle'><h1>Day 2</h1></div>
+         <div><p>Participants delved into the heart of startup development. Teams developed company models, tactics, and plans with the help of mentors, resulting in improved ideas and plans due to productive interactions, constructive feedback sessions, and insightful observations.</p></div>
         </div>
         </div>
         <div className='img2'>
@@ -43,10 +43,12 @@ const Home = () => {
         </div>
         <div className='text3'>
         <div className='heroBox'>
-         <div className='heroBoxTitle'><h1>Lorem Ipsum Dolor</h1></div>
-         <div><p>Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum DolorLorem Ipsum DolorLorem Ipsum Dolor Lorem Ipsum Dolor</p></div>
+         <div className='heroBoxTitle'><h1>Day 3</h1></div>
+         <div><p>Idea submission, pitching, and results announcement marked the event's conclusion. Teams presented their concepts to judges, who assessed them for viability, inventiveness, and success potential. Participants were enthusiastic and ingenious, and their enthusiasm and energy was evident.</p></div>
         </div>
         </div>
+      </div>
+      </div>
       </div>
 </section>
          
